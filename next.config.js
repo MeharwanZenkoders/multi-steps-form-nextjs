fbef005b255286fcfb753fb12400470ce33febbf
MeharwanZenkoders <meharwan.zenkoders@gmail.com>
2024-09-11 13:24:26 +0500

    ..

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   reactStrictMode: true,
 
   images:{
     domains:["res.cloudinary.com"]
+  },
+  eslint:{
+    ignoreDuringBuilds:true
   }
 }
 
