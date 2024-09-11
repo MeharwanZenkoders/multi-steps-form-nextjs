6460ef4be068aee8ee41e2ab6b6043e5ac46e93f
MeharwanZenkoders <meharwan.zenkoders@gmail.com>
2024-09-11 17:12:24 +0500

    ...

--- src/components/ui/ratings.tsx
+++ src/components/ui/ratings.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactStars from 'react-stars';
+import React from "react";
+import ReactStars from "react-stars";
 import { Field, FormikErrors, FormikValues, useFormikContext } from "formik";
 
 interface RatingProps {
@@ -7,15 +7,21 @@ interface RatingProps {
 }
 
 const services = [
-  { name: 'Physiotherapist' },
-  { name: 'Chiropractor' },
-  { name: 'Osteopath' },
-  { name: 'Myotherapist' },
-  { name: 'RMT' },
-  { name: 'A_TD' },
+  { name: "Physiotherapist" },
+  { name: "Chiropractor" },
+  { name: "Osteopath" },
+  { name: "Myotherapist" },
+  { name: "RMT" },
+  { name: "A_TD" },
 ];
 
-const StarRating = ({ rating, onRatingChange }: { rating: number, onRatingChange: (value: number) => void }) => {
+const StarRating = ({
+  rating,
+  onRatingChange,
+}: {
+  rating: number;
+  onRatingChange: (value: number) => void;
+}) => {
   return (
     <ReactStars
       count={5}
@@ -37,34 +43,48 @@ const ProfileForm: React.FC<RatingProps> = ({ errors }) => {
 
   return (
     // <div className="min-h-screen flex items-center justify-center">
-     <>
+    <>
+      <p className="text-gray-600 mb-6">
+        Please select which of the following you have used in the past and rate
+        how well they worked for you. (out of 5 stars){" "}
+        <span className="text-red-500">*</span>
+      </p>
 
-        <p className="text-gray-600 mb-6">
-          Please select which of the following you have used in the past and rate how well they worked for you. (out of 5 stars) <span className="text-red-500">*</span>
-        </p>
+      {services?.map((service) => (
+        <div key={service.name} className="flex flex-col mb-4">
+          <div className="flex items-center justify-between">
+            <span>{service.name}</span>
 
-        {services?.map((service) => (
-          <div key={service.name} className="flex flex-col mb-4">
-            <div className="flex items-center justify-between">
-              <span>{service.name}</span>
+            <Field name={`ratings.${service.name}`}>
+              {() => (
+                <StarRating
+                  rating={values.ratings?.[service.name] || 0}
+                  onRatingChange={(value) =>
+                    handleRatingChange(`ratings.${service.name}`, value)
+                  }
+                />
+              )}
+            </Field>
+          </div>
 
-              <Field name={`ratings.${service.name}`}>
-                {() => (
-                  <StarRating
-                    rating={values.ratings?.[service.name] || 0}
-                    onRatingChange={(value) => handleRatingChange(`ratings.${service.name}`, value)}
-                  />
-                )}
-              </Field>
-            </div>
-
-            {/* Display error message below the rating */}
+          {/* Display error message below the rating
             {typeof errors.ratings == 'object' && errors.ratings?.[service.name] && typeof errors.ratings?.[service.name] === "string" ? (
               <div className="text-xs text-red-500 mt-1">{errors.ratings?.[service.name]}</div>
-            ) : null}
-          </div>
-        ))}
-      </>
+            ) : null} */}
+
+          {/* Explicitly cast errors.ratings to correct type */}
+          {errors.ratings &&
+          typeof errors.ratings === "object" &&
+          (errors.ratings as FormikErrors<any>)[service.name] &&
+          typeof (errors.ratings as FormikErrors<any>)[service.name] ===
+            "string" ? (
+            <div className="text-xs text-red-500 mt-1">
+              {(errors.ratings as FormikErrors<any>)[service.name] as string}
+            </div>
+          ) : null}
+        </div>
+      ))}
+    </>
     // </div>
   );
 };
